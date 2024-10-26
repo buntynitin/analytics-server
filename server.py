c068ed23209d82bd4c2a1230386bea546120b14f
Nitin Chauhan <44166323+buntynitin@users.noreply.github.com>
2024-10-26 12:25:24 +0530

    Initial Commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException, Request
+from pymongo import MongoClient
+import os
+
+app = FastAPI()
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["your_database_name"]
+collection = db["analytics"]
+
+@app.post("/")
+async def analytics(request: Request):
+    try:
+        body = await request.json()        
+        collection.insert_one(body)
+        return {"data": True}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
