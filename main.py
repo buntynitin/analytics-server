0cb8d81efadf356d1b203d697322e89ed31c8027
Nitin Chauhan <44166323+buntynitin@users.noreply.github.com>
2024-11-13 00:11:16 +0530

    Added locations server

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ MONGO_URI = os.environ.get("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["analytics"]
 collection = db["analytics"]
+locationsCollection = db["locations"]
 
 @app.post("/")
 async def analytics(request: Request):
     try:
         body = await request.json()        
         collection.insert_one(body)
+        return {"data": True}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/location")
+async def add_location(request: Request):
+    try:
+        body = await request.json()        
+        locationsCollection.insert_one(body)
         return {"data": True}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
